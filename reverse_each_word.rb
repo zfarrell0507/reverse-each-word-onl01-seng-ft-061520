3500efec10c658b64d27c9a90917dbdf2acf1e90
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-25 19:21:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
-  sentence1 = "Hello there, and how are you?"
-  sentence2 = "Hi again, just making sure it's reversed!"
-
-def reverse_each_word(sentence1, sentence2)
+def reverse_each_word
+  
   sentence1 = sentence1.split.collect {|word| word.reverse}.join(" ")
   puts sentence1
    sentence1 = sentence1.split.collect {|word| word.reverse}.join(" ")
