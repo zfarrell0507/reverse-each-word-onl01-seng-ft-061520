a70aff5a6697bcf62b9c06b27b7b03f2d149d6ce
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-25 19:41:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,11 +5,13 @@
 def reverse_each_word(sentence1)
   sentence3 = sentence1.split.collect {|word| word.reverse}.join(" ")
   puts sentence3
+  sentence3
 end
   reverse_each_word(sentence1)
 
 def reverse_each_word(sentence2)
   sentence4 = sentence2.split.collect {|word| word.reverse}.join(" ")
   puts sentence4
+  sentence4
 end
   reverse_each_word(sentence2)
