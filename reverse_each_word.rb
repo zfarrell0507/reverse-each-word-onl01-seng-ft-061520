ba967695c2463660255fbb467750c16f44581ace
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-25 19:38:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,13 +3,13 @@
   sentence2 = "Hi again, just making sure it's reversed!"
 
 def reverse_each_word(sentence1)
-  sentence1 = sentence1.split.collect {|word| word.reverse}.join(" ")
-  puts sentence1
+  sentence3 = sentence1.split.collect {|word| word.reverse}.join(" ")
+  puts sentence3
 end
   reverse_each_word(sentence1)
 
-def reverse_each_word(sentence1)
-  sentence2 = sentence2.split.collect {|word| word.reverse}.join(" ")
-  puts sentence2
+def reverse_each_word(sentence2)
+  sentence4 = sentence2.split.collect {|word| word.reverse}.join(" ")
+  puts sentence4
 end
-  reverse_each_word(sentence2
+  reverse_each_word(sentence2)
