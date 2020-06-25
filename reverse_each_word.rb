208a6b246587ebb82e75a263ff807d583720074e
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-25 19:05:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,7 +14,12 @@ end
     
 
   
-  
+  sentence1 = "Hello there, and how are you?"
+def reverse_each_word(sentence1)
+  sentence1.split.collect {|word| word.reverse}.join(" ")
+  puts sentence1
+end
+  reverse_each_word(sentence1)
   
 
 
